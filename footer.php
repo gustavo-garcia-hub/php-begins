a70d3d65276c29a36677e836d108743d074adae7
Gustavo Garcia <sousagustavogarcia@hotmail.com>
2021-03-26 22:29:56 -0300

    UPDATE

---
+++ footer.php
@@ -0,0 +1,6 @@
+<?php
+    echo "<footer>";
+    echo "<p>ACESSADO EM ...</p>";
+    echo "<p>DESENVOLVIDO POR GUSTAVO GARCIA &copy; 2021</p>";
+    echo "</footer";
+    $conexao->close();
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="PT-BR">
 <head>
+    <title>GUARDA MIRIM</title>
     <meta charset="UTF-8">
-    <title>ORÇAMENTOS</title>
-    <link rel="stylesheet" href="assets\styles.css"/>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-      rel="stylesheet">
+    <link rel="stylesheet" href="assets/styles.css"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">  
 </head>
 <body>
     <?php
@@ -29,7 +30,7 @@
                             $myFormat = date("d-m-Y G:i", $time);
                             echo "<tr><td>$reg->descricao";
                             echo "<td>$reg->ordem";
-                            echo "<td>$reg->valor";
+                            echo "<td>R$ $reg->valor";
                             echo "<td>$myFormat";
                             echo "<td>adm";
                         }   
@@ -38,6 +39,6 @@
             ?>
         </table>
     </div>
-    <?php $conexao->close();?>
+    <?php include_once "footer.php"; ?>
 </body>
 </html>
